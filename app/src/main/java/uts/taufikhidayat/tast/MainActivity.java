472c85a7c3fab10fa080fcd1eea83a0e96b5dbfe
taufik <th160602@gmail.com>
2024-10-29 00:49:29 +0700

    first commit

---
+++ app/src/main/java/uts/taufikhidayat/tast/MainActivity.java
@@ -0,0 +1,23 @@
+package uts.taufikhidayat.tast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Menjalankan handler untuk berpindah ke LoginActivity setelah 2 detik (2000 ms)
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish(); // Tutup MainActivity agar tidak dapat kembali ke layar ini
+        }, 2000); // 2000 ms = 2 detik
+    }
+}
